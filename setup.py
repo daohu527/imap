c25b9d7c426ffbf55dbee69943347a84e9bd5469
daohu527 <daohu527@gmail.com>
2022-11-21 18:44:05 +0800

    feat: update version to 0.0.8

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="imap_box",
-    version="0.0.7",
+    version="0.0.8",
     author="daohu527",
     author_email="daohu527@gmail.com",
     description="High-resolution map visualization and conversion tool",
